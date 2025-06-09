d16f955fdd8af2482ca05fde542e47d86ee46b6b
MaryD <holetsmasha@ukr.net>
2025-06-09 19:55:46 +0300

    corrected files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
+import { fetchContacts } from "./redux/contacts/operations";
+import { refreshUser } from "./redux/auth/operations";
+import { selectIsRefreshing, selectIsLoggedIn } from "./redux/auth/selectors";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactForm from "./components/ContactForm/ContactForm";
-import { fetchContacts } from "./redux/contacts/operations";
 
 export default function App() {
   const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(refreshUser()); // перевірка токену і підстановка заголовку
   }, [dispatch]);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      dispatch(fetchContacts());
+    }
+  }, [dispatch, isLoggedIn]);
+
+  if (isRefreshing) return <p>Loading user...</p>;
 
   return (
     <div>
